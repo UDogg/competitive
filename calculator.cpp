5bb3afab1fdd18491f13c4bea8b6d02346fbd264
Utkarsh Choudhary <ufc5009@psu.edu>
2023-06-25 14:00:56 +0530

    made some corrections

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+float q = 0.0;
+
 int add(int x, int y)
 {
     return x + y;
@@ -16,9 +18,15 @@ int multiply(int x, int y)
     return x * y;
 }
 
-int divide(int x, int y)
+int quotient(int x, int y)
 {
-    return x / y;
+    return x/y;
+}
+
+float divide(int x, int y)
+{
+    q = float(x)/float(y);
+    return q;
 }
 
 int main()
@@ -34,7 +42,8 @@ int main()
     cout << "The sum of " << x << " and " << y << " is " << add(x, y) << endl;
     cout << "The difference of " << x << " and " << y << " is " << subtract(x, y) << endl;
     cout << "The product of " << x << " and " << y << " is " << multiply(x, y) << endl;
-    cout << "The quotient of " << x << " and " << y << " is " << divide(x, y) << endl;
+    cout << "The quotient of " << x << " and " << y << " is " << quotient(x, y) << endl;
+    cout << "The division of " << x << " and " << y << " produces " << divide(x, y) << endl;
     
     cout << "Hello World!" << endl;
     
